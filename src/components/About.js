eafd15f5176175084e56f69b3ed2542e1ce82dde
ClaytonDewey <clay@dryadmedia.com>
2023-05-31 15:19:06 -0500

    Create About component

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+  return (
+    <main>
+      <p>
+        This application contains snippets from various Codecademy articles. You
+        can browse all our articles and read them in their entirety{' '}
+        <a href='https://www.codecademy.com/articles'>here</a>.
+      </p>
+    </main>
+  );
+}
+
+export default About;
